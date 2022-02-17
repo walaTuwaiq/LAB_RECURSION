11e5d3a7eb75aaa82f270616f00300a56c9a6610
walaTuwaiq <wala.radi.rae.js@tuwaiq.edu.sa>
2022-02-17 12:15:59 +0300

    fixed

--- index.py
+++ index.py
@@ -1,16 +1,12 @@
 
-countLetter = 0
-count = 0
-def countChar(word):
-    global countLetter
-    global count
+def countChar(word,countLetter = 0,count = 0):
     word = word.lower()
     lst = list(word)
     if lst[countLetter] == "a" or lst[countLetter] == "e" or lst[countLetter] == "i" or lst[countLetter] == "o" or lst[countLetter] == "u":
         count+=1
     countLetter += 1
     if countLetter < len(word):
-        countChar(word)
+        countChar(word,countLetter,count)
     else:
         print(f"Number to vowels letters in your phrase is: {count} times.")
         return
